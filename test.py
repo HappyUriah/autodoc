6c7283055723c1b30896c1c6849873731da170a9
chengaohua <chen.gaohua@intellif.com>
2020-07-07 09:36:19 +0800

    add struct

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def extractBrief(line) :
     return line[line.find('\\brief') + len('\\brief'):].strip()
 
-def extractEnumName(line) :
+def extractName(line) :
     return line[line.find('}') + 1 : line.find(';')].strip()
 
 def extractEnumEle(line) :
@@ -28,19 +28,17 @@ def writeEnumToFile(brief, name, ele, f) :
         f.write("|" + ele[3 *i] + "|" + ele[3 * i + 1] + "|" + ele[3 * i + 2] + "|\n")
   
 
-   
-
 
 def isEnum(block):
     for line in block :
-        if 'enum' in line:
+        if ' enum ' in line:
             return True
     return False
 
 def ansisEnumBlock(block, fout):
 
     num = len(block)
-    enumName = extractEnumName(block[num-1])
+    enumName = extractName(block[num-1])
     enumEle = []
     print(enumName)
     for line in block:
@@ -57,12 +55,99 @@ def ansisEnumBlock(block, fout):
     if len(enumEle) > 0 :
         writeEnumToFile(brief, enumName, enumEle, fout)
 
+def isStruct(block):
+    for line in block :
+        if ' struct ' in line:
+            return True
+    return False
+
+def extractArrayEle(note) :
+    strs = note.split()
+
+    idx = 0
+    for str1 in strs:
+        idx = idx + 1
+        if('[' in str1) :
+            val = str1[:str1.find('[')]
+            break
+    typ = note[0: note.find(val)]
+    typ += '[]'
+    return typ, val
+
+def extractValEle(note) :
+    strs = note.split()
+    lastStr = strs[len(strs) - 1]
+    if lastStr == ";" :
+        val = strs[len(strs) - 2]
+    else :
+        val = lastStr[0:-1]
+    typ = note[0: note.find(val)]
+    return typ, val
+    
+
+
+
+
+def extractStructEle(line) :
+    keyword = '///<'
+    des = line[line.find(keyword) + len(keyword): ]
+
+    note = line[0:line.find(keyword)]
+
+    if '[' in note :
+        # 数组
+        typ, val = extractArrayEle(note)
+        return typ, val , des
+
+    else :
+        typ, val = extractValEle(note)
+        return typ, val, des
+
+    
+
+def writeStructToFile(brief, name, ele, f) :
+    f.write('### ' + name + '\n')
+    f.write('*结构体描述' + '*\n\n')
+    f.write(brief + '\n')
+    f.write(" | 类型      |    变量 | 描述  |\n| :-------- | --------:| :--: |\n")
+   
+    cnt = (int)(len(ele) / 3)
+    for i in range(0, cnt):
+        f.write("|" + ele[3 *i] + "|" + ele[3 * i + 1] + "|" + ele[3 * i + 2] + "|\n")
+  
+  
+
+
+
+def ansisStructBlock(block, fout):
+
+    num = len(block)
+    Name = extractName(block[num-1])
+    structEle = []
+    print(Name)
+    for line in block:
+        if "\\brief" in line:
+            brief = extractBrief(line)
+            print(brief)
+        elif '///<' in line and not line.startswith('//'):
+            typ, val, des = extractStructEle(line)
+            structEle.append(typ)
+            structEle.append(val)
+            structEle.append(des)
+           # print(key,des)
+    
+    print("++++++++++++++++" , len(structEle))
+
+    if len(structEle) > 0 :
+        writeStructToFile(brief, Name, structEle, fout)
+
+
 
 fout = open('output.md','w')
 
 
 
-f = open("/home/gh/Documents/workspace/arcternsdk/src/base/arcternsdk_attr.h")               # 返回一个文件对象 
+f = open("/home/gh/Documents/workspace/arcternsdk/src/base/arcternsdk_base.h")               # 返回一个文件对象 
 line = f.readline()               # 调用文件的 readline()方法 
 block = []
 remain = True
@@ -82,6 +167,8 @@ while line:
 
         if isEnum(block) :
             ansisEnumBlock(block,fout)
+        elif isStruct(block) :
+            ansisStructBlock(block, fout)
         block.clear()
 
 
