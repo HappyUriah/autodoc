4f93bc5f07fd73135bc4a216500d27219692fe70
chengaohua <chen.gaohua@intellif.com>
2020-07-07 13:56:45 +0800

    split file

---
+++ enum.py
@@ -0,0 +1,83 @@
+def extractBrief(lines) :
+    brief = ""
+
+    for line in lines:
+
+        if line.find("\\brief") != -1:
+            brief += line[line.find('\\brief') + len('\\brief'):].strip()
+
+        else :
+            brief += line.strip('/\n ')
+        brief +=' '
+    return brief
+
+def extractName(line) :
+    return line[line.find('}') + 1 : line.find(';')].strip()
+
+def extractEnumEle(line) :
+    keyword = '///<'
+    des = line[line.find(keyword) + len(keyword): ]
+    strs = line.split()
+    key = strs[0];
+
+    if strs[1].strip() == "=" :
+        value = strs[2].strip(',')
+    else :
+        value = strs[1][1:].strip(',')
+    
+    return key, value, des
+    
+def writeEnumToFile(brief, name, ele, f) :
+    f.write('### ' + name + '\n')
+    f.write('*枚举描述' + '*\n\n')
+    f.write(brief + '\n')
+    f.write(" | 枚举名      |    枚举值 | 描述  |\n| :-------- | --------:| :--: |\n")
+   
+    cnt = (int)(len(ele) / 3)
+    for i in range(0, cnt):
+        f.write("|" + ele[3 *i] + "|" + ele[3 * i + 1] + "|" + ele[3 * i + 2] + "|\n")
+  
+
+
+def isEnum(block):
+    for line in block :
+        if 'enum ' in line:
+            return True
+    return False
+
+def ansisEnumBlock(block, fout):
+
+    num = len(block)
+    enumName = extractName(block[num-1])
+    enumEle = []
+    print(enumName)
+    briefIdx = []
+    for idx in range(0, num):
+        line = block[idx]
+        if "\\brief" in line:
+            print(idx)
+            briefIdx.append(idx)
+        elif line.strip().startswith('///') :
+            briefIdx.append(idx);
+        elif '///<' in line and not line.startswith('//'):
+            key,value,des = extractEnumEle(line)
+            enumEle.append(key)
+            enumEle.append(value)
+            enumEle.append(des)
+            print(key,value, des)
+    
+    print(briefIdx)
+
+    start = briefIdx[0]
+    briefs=[]
+    for idx in briefIdx:
+        if idx == start:
+            briefs.append(block[idx])
+        else:
+            break
+        start +=1
+
+    brief = extractBrief(briefs)
+
+    if len(enumEle) > 0 :
+        writeEnumToFile(brief, enumName, enumEle, fout)
---
+++ struct.py
@@ -0,0 +1,121 @@
+def extractBrief(lines) :
+    brief = ""
+
+    for line in lines:
+
+        if line.find("\\brief") != -1:
+            brief += line[line.find('\\brief') + len('\\brief'):].strip()
+
+        else :
+            brief += line.strip('/\n ')
+        brief +=' '
+    return brief
+
+def extractName(line) :
+    return line[line.find('}') + 1 : line.find(';')].strip()
+    
+def isStruct(block):
+    for line in block :
+        if 'struct ' in line:
+            return True
+    return False
+
+def extractArrayEle(note) :
+    strs = note.split()
+
+    idx = 0
+    for str1 in strs:
+        idx = idx + 1
+        if('[' in str1) :
+            val = str1[:str1.find('[')]
+            break
+    typ = note[0: note.find(val)]
+    typ += '[]'
+    return typ, val
+
+def extractValEle(note) :
+    strs = note.split()
+    lastStr = strs[len(strs) - 1]
+    if lastStr == ";" :
+        val = strs[len(strs) - 2]
+    else :
+        val = lastStr[0:-1]
+    typ = note[0: note.find(val)]
+    return typ, val
+    
+
+
+
+
+def extractStructEle(line) :
+    keyword = '///<'
+    des = line[line.find(keyword) + len(keyword): ]
+
+    note = line[0:line.find(keyword)]
+
+    if '[' in note :
+        # 数组
+        typ, val = extractArrayEle(note)
+        return typ, val , des
+
+    else :
+        typ, val = extractValEle(note)
+        return typ, val, des
+
+    
+
+def writeStructToFile(brief, name, ele, f) :
+    f.write('### ' + name + '\n')
+    f.write('*结构体描述' + '*\n\n')
+    f.write(brief + '\n')
+    f.write(" | 类型      |    变量 | 描述  |\n| :-------- | --------:| :--: |\n")
+   
+    cnt = (int)(len(ele) / 3)
+    for i in range(0, cnt):
+        f.write("|" + ele[3 *i] + "|" + ele[3 * i + 1] + "|" + ele[3 * i + 2] + "|\n")
+  
+  
+
+
+
+def ansisStructBlock(block, fout):
+
+    num = len(block)
+    Name = extractName(block[num-1])
+    structEle = []
+    print(Name)
+
+    briefIdx = []
+    for idx in range(0, num):
+        line = block[idx]
+        if "\\brief" in line:
+            briefIdx.append(idx)
+        elif line.strip().startswith('///') :
+            briefIdx.append(idx);
+            #brief = extractBrief(line)
+            #print(brief)
+        elif '///<' in line and not line.startswith('///'):
+            typ, val, des = extractStructEle(line)
+            structEle.append(typ)
+            structEle.append(val)
+            structEle.append(des)
+
+    
+    
+    print("==================" , briefIdx)
+
+    start = briefIdx[0]
+    briefs=[]
+    for idx in briefIdx:
+        if idx == start:
+            briefs.append(block[idx])
+        else:
+            break
+        start +=1
+
+    brief = extractBrief(briefs)
+    
+    #print("++++++++++++++++" , len(structEle))
+
+    if len(structEle) > 0 :
+        writeStructToFile(brief, Name, structEle, fout)
